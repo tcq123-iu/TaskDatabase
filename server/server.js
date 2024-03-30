09732955f8b9f73b119bd0841c2359b91a4faac2
tuilatri <trihaminh2004@gmail.com>
2024-03-30 21:24:27 +0700

    Tri_connectserver(y)database(n)

--- server/server.js
+++ server/server.js
@@ -1,27 +1,32 @@
-import express from "express"
-import mysql from "mysql2"
+// import express from "express"
+// import mysql from "mysql2"
+// import cors from "cors"
+
+const express = require ('express');
+const mysql = require ('mysql2');
+const cors = require ('cors');
 
 const app = express()
 
-const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"12345",
-    database:"task"
-})
+app.use(cors())
 
-db.connect();
-db.query('SELECT * FROM Example', function (error, results, field) {
-    if (error) throw error;
-    console.log(results)
-})
+// const db = mysql.createConnection({
+//     host:"localhost",
+//     user:"root",
+//     password:"12345",
+//     database:"task"
+// })
+
+// db.connect();
+// db.query('SELECT * FROM Example', function (error, results, field) {
+//     if (error) throw error;
+//     console.log(results)
+// })
 
 app.get("/", (req,res)=> {
-    res.json("hello this is backend")
+    return res.json("hello this is backend");
 })
 
-
-
 app.listen(8800, ()=> {
-    console.log("Connected to backend")
+    console.log("Connected to backend");
 })
