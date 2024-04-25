c1024c231db8bc89eb70059c677e28272c9d6c5b
ThanhHung1912 <itcsiu22051@student.hcmiu.edu.vn>
2024-04-25 22:02:11 +0700

    Add database file

--- client/src/components/pages/HomePage/HomePage.jsx
+++ client/src/components/pages/HomePage/HomePage.jsx
@@ -10,8 +10,7 @@ import { useSelector } from 'react-redux'
 const HomePage = () => {
     const dispatch = useDispatch()
     const navigate = useNavigate()
-    const selector = useSelector()
-    console.log(selector)
+    
     const [openModal, setOpenModal] = useState(false);
     const handleModalClose = () => {
         setOpenModal(false);
@@ -24,9 +23,6 @@ const HomePage = () => {
                 <AddBoard onClick={() => setOpenModal(true)}>
                     Create new board
                 </AddBoard>
-                <Button onClick={() => dispatch(test("concacccccc"))}>
-                    Test
-                </Button>
             </Wrapper>
         </Container>
   )
